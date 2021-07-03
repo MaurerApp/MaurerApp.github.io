27567078856b4d2f01b0e6c9d29c63ffefaec3e9
Maurer Michael <michael.maurer1991@outlook.de>
2021-07-03 21:57:03 +0200

    WIP

---
+++ src/scripts/hero.js
@@ -0,0 +1,30 @@
+const heroElement = document.getElementById("hero")
+
+heroElement.addEventListener("mouseover", moveMouse)
+
+let lastX, lastY, lastD, lastDx, lastDy
+
+reset()
+
+window.onresize = function () {
+    reset()
+}
+
+function reset() {
+    lastX = 0
+    lastY = 0
+    lastD = 0
+    lastDx = 0
+    lastDy = 0
+}
+
+function moveMouse(e) {
+
+    lastD += Math.sqrt(
+        Math.pow(e.clientX - lastX, 2) + Math.pow(e.clientY - lastY, 2)
+    );
+
+    lastX = e.clientX;
+    lastY = e.clientY;
+
+}
